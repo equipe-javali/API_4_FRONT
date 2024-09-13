c1477ba4c552b9d7d10d55f0fad80724ca49ac33
Katiane <katiane.soares@fatec.sp.gov.br>
2024-09-13 10:43:25 -0300

    42 - Criação da estrutura da página cadastrar estação

--- api4/src/index.tsx
+++ api4/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { CadastroEstacao } from './pages/CadastroEstacao';
 
+const estacoesData = [
+  { id: 1, nome: 'Estação 1', foto: 'https://via.placeholder.com/150' },
+  { id: 2, nome: 'Estação 2', foto: 'https://via.placeholder.com/150' },
+  // ... mais estações
+];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
-
-
+root.render(<CadastroEstacao />);
