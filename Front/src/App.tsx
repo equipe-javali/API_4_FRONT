1411e650403695c540d308b3077f462ebcdeb125
Katiane <katiane.soares@fatec.sp.gov.br>
2024-09-17 16:37:56 -0300

    42-Feita a integracao do front com o back da pagina cadastrar estacao

---
+++ Front/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CadastroEstacao } from './pages/CadastroEstacao';
+import BaseLateralHeader from './components/BaseLateralHeader';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<BaseLateralHeader />}>
+          <Route path="/estacao" element={<CadastroEstacao />} />
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
