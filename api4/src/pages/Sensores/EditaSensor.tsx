f01dd461593ed10f3923b144b7e106d6e98eaab2
Katiane <katiane.soares@fatec.sp.gov.br>
2024-09-25 23:07:56 -0300

    refactor: ajuste nos estilos do menu

--- api4/src/pages/Sensores/EditaSensor.tsx
+++ api4/src/pages/Sensores/EditaSensor.tsx
@@ -132,9 +132,10 @@ export function EditaSensor() {
               <div className="form-group">
                 <label className="text-wrapper">Parâmetros</label>
                 <Select
+                  isMulti
                   name="id_parametro"
                   options={parametroOptions}
-                  className="basic-single-select"
+                  className="basic-multi-select"
                   classNamePrefix="select"
                   onChange={handleSelectChange}
                   value={parametroOptions.find(option => option.value === formData.id_parametro)}
