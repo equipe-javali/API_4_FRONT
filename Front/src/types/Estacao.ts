dec3dfb90cc8e233cd3fd8691af7f4ab0108c0d1
Katianefatec <katiane.soares@fatec.gov.br>
2024-09-30 09:40:25 -0300

    remove: Exclusao de diretorio nao utilizado

--- Front/src/types/Estacao.ts
+++
@@ -1,9 +0,0 @@
-export interface CadastrarEstacao {
-  nome: string;
-  endereco: string;
-  latitude: number;
-  longitude: number;
-  mac_address: string;
-  id?: number;
-  parametros?: string;
-};
