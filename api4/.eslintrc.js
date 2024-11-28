9e1bdb3dd6eee40303cf637e693c10260efb5599
Marcus Betti <marcusbetti@gmail.com>
2024-11-28 20:00:46 -0300

    fix - inclusao de es lint no projeto e sua inclusao bo script de unit tests

---
+++ api4/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-extra-semi': 'off',
+    'no-console': 'off',
+    'no-debugger': 'warn',
+    'no-duplicate-imports': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'no-useless-catch': 'warn',
+    'react/jsx-key': 'off',
+    '@typescript-eslint/no-empty-function': 'warn',
+    'no-empty-function': 'off'
+  }
+}; 
